1b98936c483c5436fdee003e07615039cf018fdf
Zakaria Imzilen <zakariyaimzilen@gmail.com>
2023-07-22 21:18:36 +0100

    Navbar is working on Desktop & Mobile screens

---
+++ src/assets/icons.jsx
@@ -0,0 +1,67 @@
+export const barsIcon = (d) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 24 24"
+		strokeWidth={1.5}
+		stroke="currentColor"
+		className={d}
+	>
+		<path
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			d="M3.75 9h16.5m-16.5 6.75h16.5"
+		/>
+	</svg>
+);
+
+export const closeIcon = (d) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 24 24"
+		strokeWidth={1.5}
+		stroke="currentColor"
+		className={d}
+	>
+		<path
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			d="M6 18L18 6M6 6l12 12"
+		/>
+	</svg>
+);
+
+export const cartIcon = (d) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 24 24"
+		strokeWidth={1.5}
+		stroke="currentColor"
+		className={d}
+	>
+		<path
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
+		/>
+	</svg>
+);
+
+export const accountIcon = (d) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		fill="none"
+		viewBox="0 0 24 24"
+		strokeWidth={1.5}
+		stroke="currentColor"
+		className={d}
+	>
+		<path
+			strokeLinecap="round"
+			strokeLinejoin="round"
+			d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+		/>
+	</svg>
+);
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
+import Logo from "../assets/ebay.png";
+import { accountIcon, barsIcon, cartIcon, closeIcon } from "../assets/icons";
 
 const Navbar = () => {
+	const [openMenu, setOpenMenu] = useState(false);
+
 	return (
-		<nav className="m-0 py-2">
-			<div className="w-full md:max-w-screen-lg"></div>
+		<nav className="m-0 py-4 overflow-x-hidden">
+			<div className="w-full md:max-w-screen-lg md:mx-auto px-5 flex justify-between items-center">
+				<div>
+					<img width={50} src={Logo} alt="Logo" />
+				</div>
+
+				<div className="hidden md:flex gap-14 font-oswald">
+					<button className="flex gap-2 justify-center items-center hover:scale-90 transition-all hover:text-blue-400">
+						<div>{cartIcon("w-8 h-8")}</div>
+						<span className="text-md">Cart</span>
+					</button>
+					<button className="flex gap-2 justify-center items-center hover:scale-90 transition-all hover:text-blue-400">
+						<div>{accountIcon("w-8 h-8")}</div>
+						<span className="text-md">Account</span>
+					</button>
+				</div>
+
+				{/* MOBILE */}
+				<button
+					type="button"
+					className="md:hidden text-blue-600 transition-all"
+					onClick={() => setOpenMenu(true)}
+				>
+					{barsIcon("w-8 w-8")}
+				</button>
+				<div
+					className={`absolute ${
+						openMenu ? "right-0 top-0" : "-right-full -top-full rounded-es-full"
+					} h-screen w-screen py-10 px-10 z-10 bg-blue-400 transition-all duration-700 ease-in-out`}
+				>
+					<div className="w-full text-end">
+						<button type="button" onClick={() => setOpenMenu(false)}>
+							{closeIcon("w-8 w-8")}
+						</button>
+					</div>
+					<div className="absolute right-0 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col gap-12 font-oswald">
+						<button className="w-full flex gap-6 justify-center text-white">
+							<div>{cartIcon("w-8 h-8")}</div>
+							<span className="text-2xl">Cart</span>
+						</button>
+						<button className="w-full flex gap-6 justify-center text-white">
+							<div>{accountIcon("w-8 h-8")}</div>
+							<span className="text-2xl">Account</span>
+						</button>
+					</div>
+				</div>
+				{/* </-- MOBILE --> */}
+			</div>
 		</nav>
 	);
 };
